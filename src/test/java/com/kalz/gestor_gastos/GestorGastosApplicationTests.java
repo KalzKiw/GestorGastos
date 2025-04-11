2b5c0809ef6b76b444c483d8817380544ebf7553
Kalz <alejandrobrito31@gmail.com>
2025-04-11 03:13:30 +0100

    Primer commit: estructura inicial

---
+++ src/test/java/com/kalz/gestor_gastos/GestorGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalz.gestor_gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
